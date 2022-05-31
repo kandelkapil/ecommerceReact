40cdad15afe600aa14c0dd5a99c25c83c32f91e5
Kapil Kandel <kandelkapil01@gmail.com>
2022-05-31 18:40:43 +0545

    product styled

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Products from './components/Products';
 import datas from './data.json'
 
-class App extends React.Component() {
+class App extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            products: datas.products,
+            size: "",
+            sort: "",
+        }
+    }
 
     render() {
+
+
         return (
             <div className="grid-container" >
                 <header>
@@ -11,7 +23,14 @@ class App extends React.Component() {
                 </header>
 
                 <main>
-                    Product List
+                    <div className="content">
+                        <div className="main">
+                            <Products product={this.state.products} />
+                        </div>
+                        <div className="sidebar">
+                            Cart Items
+                        </div>
+                    </div>
                 </main>
                 <footer>
                     All right reserved
