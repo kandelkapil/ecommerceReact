015d52427d0db8a01a75726f91ee9c5e97fcd3da
Kapil Kandel <kandelkapil01@gmail.com>
2022-06-01 15:35:22 +0545

    add to cart feature added

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,20 +4,51 @@ export default class Cart extends Component {
     render() {
         const { cartItems } = this.props
         return (
+
             <div>
 
                 {cartItems.length === 0
                     ?
-                    <div className='cart cart-header'>
+                    (<div className='cart cart-header'>
                         Cart is Empty
-                    </div>
+                    </div>)
                     :
-                    <div className='cart cart-header'>
+                    (<div className='cart cart-header'>
                         You have {cartItems.length} in the cart
-                    </div>
-
+                    </div>)
                 }
+
+                <div>
+                    <div className="cart">
+                        <ul className="cart-items">
+                            {
+                                cartItems.map(item => (
+                                    <li key={item._id}>
+                                        <div>
+                                            <img src={item.image} alt={item.title} />
+                                        </div>
+                                        <div>
+                                            <div>
+                                                {item.title}
+                                            </div>
+                                            <button
+                                                onClick={() => this.props.removeFromCart(item)}
+                                            >
+                                                Remove
+                                            </button>
+                                        </div>
+
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                </div>
             </div>
+
+
+
         )
-    }
+    };
+
 }
