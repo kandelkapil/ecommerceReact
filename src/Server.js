6e8ab0d8559377bd881ca87b004c6b7af26a53f9
Kapil Kandel <kandelkapil01@gmail.com>
2022-06-02 18:25:58 +0545

    order api created

--- src/Server.js
+++ src/Server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const shortid = require('shortid');
+const cors = require("cors");
+
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors())
+
 
 const port = process.env.PORT || 5000;
 
@@ -41,6 +45,45 @@ app.delete("/api/products/:id", async (req, res) => {
     const deleteProduct = await Product.findByIdAndDelete(req.params.id)
     res.send(deleteProduct)
 })
+
+const Order = mongoose.model('Order', new mongoose.Schema({
+    _id: {
+        type: String,
+        default: shortid.generate
+    },
+    email: String,
+    name: String,
+    address: String,
+    total: Number,
+    cartItems: [{
+
+        _id: String,
+        title: String,
+        price: Number,
+        count: Number
+    }]
+
+}
+    , {
+        timestamps: true,
+    }
+))
+
+app.post('/api/orders', async (req, res) => {
+    if (!req.body.name ||
+        !req.body.email ||
+        !req.body.address ||
+        !req.body.cartItems ||
+        !req.body.total) {
+        return res.send({ message: 'Data is Required' })
+    }
+
+    const order = await Order(req.body).save();
+    res.send(order)
+})
+
+
+
 
 app.listen(port, () => {
     console.log(`Server Started on http://localhost:${port}`);
